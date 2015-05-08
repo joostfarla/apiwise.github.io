1a875f25775a8e7170a1ff23e51ecafdedc7a33e
Dimitri van Hees <dimitri@apiwise.nl>
2015-05-08 15:56:30 +0200

    Assets

---
+++ web/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Apiwise</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Open+Sans:700" rel="stylesheet" type="text/css">
+    <link href="/css/style.css" rel="stylesheet">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+</head>
+<body>
+    <!-- Google Tag Manager -->
+    <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KK95QR"
+                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KK95QR');</script>
+    <!-- End Google Tag Manager -->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <img src="images/apiwise.png" class="img-responsive" alt="Apiwise">
+            </div>
+            <div class="col-md-6 col-md-offset-3">
+                <p class="text-center"><a href="mailto:info@apiwise.nl">info@apiwise.nl</a> | KVK&nbsp;62956787 | BTW&nbsp;855028282B01</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
