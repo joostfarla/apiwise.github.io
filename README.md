53b19fe9cb6a9ffa72647dc34cbd5d3b44764713
Joost Farla <joost@apiwise.nl>
2015-05-27 14:52:43 +0200

    Revamped website

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# apiwise
+# Apiwise Website
+
+## Develop
+
+To serve the website from the [Vagrant Devbox](https://bitbucket.org/apiwise/devbox), run the following command:
+
+```
+jekyll serve --host vagrant
+```
+
+Now you can visit the webserver in your browser:
+
+```
+http://vagrant:4000/
+```
---
+++ js/init.js
@@ -0,0 +1,15 @@
+var anchorScroll = function () {
+    var root = $('html, body');
+
+    $('a[href*=#]:not([href=#])').click(function() {
+        root.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+
+        return false;
+    });
+};
+
+$(function() {
+    anchorScroll();
+});
